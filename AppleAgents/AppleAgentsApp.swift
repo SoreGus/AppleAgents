9f08825c96329085d627b58b57f808910452b123
Gustavo Luis Sore <sore.gustavo@gmail.com>
2026-09-23 17:26:21 -0300

    first commit

---
+++ AppleAgents/AppleAgentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleAgentsApp.swift
+//  AppleAgents
+//
+//  Created by Gustavo Soré on 23/09/26.
+//
+
+import SwiftUI
+
+@main
+struct AppleAgentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
